d4bae885a0d61fe6256dcc1fc89ed3e3cc2fd1dc
jeddaa <fathiato@gmail.com>
2022-11-03 02:55:41 +0100

    searching algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,35 +11,36 @@ void print_i(int *array, size_t i)
 }
 
 /**
- * interpolation_search - like binary search, but use idx based on formula
- * @array: given array of ints
- * @size: size of array
- * @value: value to search for
- * Return: index at which value's found
+ * interpolation_search - function to search for a value  in an array
+ * similar to binary search but uses a formula
+ * @array: array to be searched through
+ * @size: length of the array
+ * @value: value to be searched for
+ * Return: value if found or -1 if array is null or value not found
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t l = 0;
-	size_t r = size - 1;
-	size_t pos;
+	size_t lo = 0;
+	size_t hi = size - 1;
+	size_t pos = lo + (((double)(hi - lo) / (array[hi] - array[lo]))
+			* (value - array[lo]));
 
-	if (array == NULL)
+	if (!array || size == 0)
 		return (-1);
 
-	pos = l + (((double)(r - l) / (array[r] - array[l]))
-		   * (value - array[l]));
 	while (pos < size)
 	{
-		print_i(array, pos);
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
-		else if (array[pos] < value)
-			l = pos + 1;
+		else if (array[pos] > value)
+			hi = pos - 1;
 		else
-			r = pos - 1;
-		pos = l + (((double)(r - l) / (array[r] - array[l]))
-			   * (value - array[l]));
+			lo = pos + 1;
+		pos = lo + (((double)(hi - lo) / (array[hi] - array[lo]))
+				* (value - array[lo]));
 	}
-	printf("Value checked array[%lu] is out of range\n", pos);
+	printf("Value checked array[%ld] is out of range\n", pos);
 	return (-1);
 }
